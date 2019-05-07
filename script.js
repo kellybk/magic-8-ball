e6358140176f10a2c7f5c14a61764e43f50ad844
kellybk <kellybk@hotmail.com>
2019-05-06 21:35:52 -0400

    First commit

---
+++ script.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+ 
+    var magic8Ball = {};
+    
+    magic8Ball.listOfAnswers = ["No", "Yes", "I don't think so…", "Of course!", "Indubitably", "In your dreams."];
+	
+	$("#answer").hide();
+ 
+    magic8Ball.askQuestion = function(question){
+        
+		$("#answer").hide();
+		
+		$("8ball").effect("shake");
+$("8ball").attr("src","https://s3.amazonaws.com/media.skillcrush.com/skillcrush/wp-content/uploads/2016/09/answerside.png");
+		
+		setTimeout(
+			function() {
+				var question = prompt("Ask a yes or no question");
+				magic8Ball.askQuestion(question);
+			}, 500);
+		
+		$("#answer").fadeIn(7000);
+		
+		var randomNumber = Math.random();
+        var randomNumberForListOfAnswers = randomNumber * this.listOfAnswers.length;
+        var randomIndex = Math.floor(randomNumberForListOfAnswers);
+        var answer = this.listOfAnswers[randomIndex];
+ 
+        $("#answer").text(answer);
+ 
+        console.log(question);
+        console.log(answer);
+    };
+ 
+    var onClick = function() {
+        var question = prompt("ASK A YES/NO QUESTION!");
+        magic8Ball.askQuestion(question);
+    };
+ 
+    $("#questionButton").click( onClick );
+ 
+});
